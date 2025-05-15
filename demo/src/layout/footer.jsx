a98ddcdf07e6749cac40ed44aea362e443f12a84
aimwave <kutsov.akutsov@ya.ru>
2025-05-15 23:55:19 +0300

    files

---
+++ demo/src/layout/footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return <footer className="page-footer teal lighten-1">
+    <div className="footer-copyright">
+      <div className="container">
+        © {new Date().getFullYear()} aimwave
+        <a className="grey-text
+        text-lighten-4 right" 
+        href="!#">Repository</a>
+      </div>
+    </div>
+</footer>
+}
+export { Footer };
